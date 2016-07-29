79d634087d1c4aec2a8df4db67e5cd034ce68c29
Satheesh-freshdesk <satheeshn@freshdesk.com>
2016-07-29 15:29:48 +0530

    print statement

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,7 @@ class StringCalculator
     if input.empty?
       0
     else
+      puts"hai"
       numbers = input.split(",").map { |num| num.to_i }
       numbers.inject(0) { |sum, number| sum + number }
     end
