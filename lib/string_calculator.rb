20e68e2ce3a9bb00eb11ba37183c6f978a607940
Satheesh-freshdesk <satheeshn@freshdesk.com>
2016-07-29 15:41:42 +0530

    3_commite

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,7 @@ class StringCalculator
     if input.empty?
       0
     else
+    	1
       numbers = input.split(",").map { |num| num.to_i }
       numbers.inject(0) { |sum, number| sum + number }
     end
